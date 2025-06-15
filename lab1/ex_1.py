c7b517b5aadd6702664fbc48357e36c518a55715
akaptsov <ca.kptsv@gmail.com>
2025-06-15 20:36:23 +0300

    Added 1st ex from lab1

---
+++ lab1/ex_1.py
@@ -0,0 +1,14 @@
+import turtle
+
+''' This program draws a letter "S" using turtle '''
+
+turtle.shape('turtle')
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)

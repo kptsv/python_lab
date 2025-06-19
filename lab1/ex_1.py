2058940e42578357b0efce9d5ece46f64d93ebe1
akaptsov <ca.kptsv@gmail.com>
2025-06-19 16:42:14 +0300

    Done exercise 3

--- lab1/ex_1.py
+++ lab1/ex_1.py
@@ -1,14 +1,29 @@
 import turtle
 
-''' This program draws a letter "S" using turtle '''
+def do_ex2():
+    ''' This function draws a letter "S" using turtle '''
+    turtle.shape('turtle')
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(50)
 
-turtle.shape('turtle')
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
-turtle.right(90)
-turtle.forward(50)
-turtle.right(90)
-turtle.forward(50)
+
+def do_ex3():
+    ''' Draw square using turtle'''
+    turtle.shape('turtle')
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+
+if __name__ == '__main__':
+    do_ex3()

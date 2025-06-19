600718557b3954cad21e95a0f6d997c09c7d1afd
akaptsov <ca.kptsv@gmail.com>
2025-06-19 17:05:04 +0300

    Done ex4

--- lab1/ex_1.py
+++ lab1/ex_1.py
@@ -25,5 +25,18 @@ def do_ex3():
     turtle.left(90)
     turtle.forward(100)
 
+
+def do_ex4():
+    ''' Draw circle using turtle'''
+    side_length = 5
+    n = 100 # многоупольник с большим количеством сторон, приближается к окружности
+    angle_to_turn = 360 / n
+    turtle.shape('turtle')
+    for i in range(n):
+        turtle.forward(side_length)
+        turtle.left(angle_to_turn)
+    turtle.done()
+
+
 if __name__ == '__main__':
-    do_ex3()
+    do_ex4()
